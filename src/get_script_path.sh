0c110c8ae9635d3501ecd9cf39879070e6f37075
Neder Alfonso Fandino Andrade <neder.fandino@salesdecameron.com>
2024-08-17 21:46:41 -0500

    feat: Improve script path extraction in get_script_path.sh
    The `get_script_path` function in `src/get_script_path.sh` has been significantly enhanced. The changes include:
    
    - Replacing the use of `sed` for stripping 'source' and comments with bash string manipulation, which improves performance and readability.
    - Adding a condition to only remove 'source' if it is at the beginning of the line followed by a space, preventing potential false positives.
    - Trimming trailing spaces from the final path before returning it. This ensures that paths are clean and free from unnecessary whitespace.

--- src/get_script_path.sh
+++ src/get_script_path.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-get_script_path()
-{
-    local line
-    line="$1"
-    # shellcheck disable=SC2001
-    import=$(echo "$line" | sed 's/source\s\s*//g')
-    # shellcheck disable=SC2001
-    import=$(echo "$import" | sed 's/\s*#.*$//g')
-    echo "$import"
+get_script_path() {
+    local input_line="$1"
+    local stripped_line
+    local comment_removed_line
+    local final_path
+
+    # Remove 'source' only if it is at the beginning of the line, followed by a space
+    if [[ "$input_line" =~ ^source[[:space:]]+ ]]; then
+        stripped_line="${input_line#source }"
+    else
+        stripped_line="$input_line"
+    fi
+
+    # Remove anything after and including the first occurrence of a '#'
+    comment_removed_line="${stripped_line%%#*}"
+
+    # Trim any trailing spaces
+    final_path="$(echo -e "${comment_removed_line}" | sed -e 's/[[:space:]]*$//')"
+
+    echo "$final_path"
 }
